29e37bcb341704a58398e17d48b31a042f5f83b0
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-14 22:28:01 +0100

    Add test file for calculator

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,34 @@
+import calculate from "./calculate";
+import chai from "chai";
+
+
+chai.config.truncateThreshold = 0;
+const expect = chai.expect;
+
+
+
+function pressedButtons(buttons) {
+  const value = {};
+  buttons.forEach((button) => {
+    Object.assign(value, calculate(value, button));
+  });
+  
+  Object.keys(value).forEach((key) => {
+    if (value[key] === null) {
+      delete value[key];
+    }
+  });
+  return value;
+}
+function expectedButtons(buttons, expectations) {
+  expect(pressedButtons(buttons)).to.deep.equal(expectations);
+}
+function test(buttons, expectations, only = false) {
+  const func = only ? it.only : it;
+  func(`buttons ${buttons.join("")}->${JSON.stringify(expectations)}`, () => {
+    expectedButtons(buttons, expectations);
+  });
+}
+describe("calculate", function () {
+  test(["6"], { next: "6" });
+});
