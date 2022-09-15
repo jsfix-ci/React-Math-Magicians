fbc12f840c7f16251301244f4f2582bd6c72192a
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-15 18:55:32 +0200

    Test the operate

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,35 +1,35 @@
 import operate from './operate';
 
-describe('Unit Test For operate', () => {
-  test('test for subtracting numbers', () => {
-    const num1 = 35;
-    const num2 = 15;
+describe('Testing the operate', () => {
+  test('test subscription', () => {
+    const firstNumber = 20;
+    const secondNumber = 1;
     const operator = '-';
-    const result = operate(num1, num2, operator);
-    expect(result).toBe('20');
+    const result = operate(firstNumber, secondNumber, operator);
+    expect(result).toBe('19');
   });
 
-  test('test for adding numbers', () => {
-    const num1 = 15;
-    const num2 = 15;
+  test('test addition', () => {
+    const firstNumber = 4;
+    const secondNumber = 3;
     const operator = '+';
-    const result = operate(num1, num2, operator);
-    expect(result).toBe('30');
+    const result = operate(firstNumber, secondNumber, operator);
+    expect(result).toBe('7');
   });
 
-  test('test for multiplying numbers', () => {
-    const num1 = 5;
-    const num2 = 15;
+  test('test multiplication', () => {
+    const firstNumber = 3;
+    const secondNumber = 4;
     const operator = 'x';
-    const result = operate(num1, num2, operator);
-    expect(result).toBe('75');
+    const result = operate(firstNumber, secondNumber, operator);
+    expect(result).toBe('12');
   });
 
-  test('test for dividing numbers', () => {
-    const num1 = 35;
-    const num2 = 7;
+  test('test division', () => {
+    const firstNumber = 8;
+    const secondNumber = 2;
     const operator = '÷';
-    const result = operate(num1, num2, operator);
-    expect(result).toBe('5');
+    const result = operate(firstNumber, secondNumber, operator);
+    expect(result).toBe('4');
   });
 });
