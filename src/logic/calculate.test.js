e3495cb3e017d5341a5018248b120b3ae05249fa
Diego <godie8a@hotmail.com>
2022-09-15 14:34:40 -0500

    fix eslint errors

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,18 +1,15 @@
-import calculate from "./calculate";
-import chai from "chai";
-
+import chai from 'chai';
+import calculate from './calculate';
 
 chai.config.truncateThreshold = 0;
-const expect = chai.expect;
-
-
+const { expect } = chai;
 
 function pressedButtons(buttons) {
   const value = {};
   buttons.forEach((button) => {
     Object.assign(value, calculate(value, button));
   });
-  
+
   Object.keys(value).forEach((key) => {
     if (value[key] === null) {
       delete value[key];
@@ -25,10 +22,10 @@ function expectedButtons(buttons, expectations) {
 }
 function test(buttons, expectations, only = false) {
   const func = only ? it.only : it;
-  func(`buttons ${buttons.join("")}->${JSON.stringify(expectations)}`, () => {
+  func(`buttons ${buttons.join('')}->${JSON.stringify(expectations)}`, () => {
     expectedButtons(buttons, expectations);
   });
 }
-describe("calculate", function () {
-  test(["6"], { next: "6" });
+describe('calculate', () => {
+  test(['6'], { next: '6' });
 });
